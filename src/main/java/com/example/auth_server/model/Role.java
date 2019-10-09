b3bb1ea1edf89e5c6da038ad24a8e45c9ec4745b
muilat <muilat.champ@gmail.com>
2019-10-09 17:55:36 +0100

    authorization well server defined

--- src/main/java/com/example/auth_server/model/Role.java
+++ src/main/java/com/example/auth_server/model/Role.java
@@ -24,5 +24,27 @@ public class Role implements Serializable {
             @JoinColumn(name = "permission_id", referencedColumnName = "id")})
     private List<Permission> permissions;
 
+    public Integer getId() {
+        return id;
+    }
 
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Permission> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(List<Permission> permissions) {
+        this.permissions = permissions;
+    }
 }
